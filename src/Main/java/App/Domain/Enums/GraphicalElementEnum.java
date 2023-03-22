31bdead35e3324c3e254cd333c67795b419f4c3a
martinsiitam <martin.siitam@hotmail.com>
2023-03-22 19:31:33 +0100

    new tiles enum fix- Martin

--- src/Main/java/App/Domain/Enums/GraphicalElementEnum.java
+++ src/Main/java/App/Domain/Enums/GraphicalElementEnum.java
@@ -3,6 +3,10 @@ package App.Domain.Enums;
 public enum GraphicalElementEnum {
 
     DEFAULT_FLOOR("../Resources/Tiles/floor.png"),
+
+    OBSTACLE_UNKNOWN_EXPLOSIVE("../Resources/Tiles/UnknownExplosiveTile.png"),
+    OBSTACLE_EXPLOSIVE_ACID("../Resources/Tiles/ExplosiveAcidTile.png"),
+    OBSTACLE_EXPLOSIVE_RADIATION("../Resources/Tiles/ExplosiveRadiationTile.png"),
     OBSTACLE_ACID("../Resources/Tiles/acid.png"),
     OBSTACLE_PIT("../Resources/Tiles/pit.png"),
     OBSTACLE_RADIATION("../Resources/Tiles/radiation.png"),
