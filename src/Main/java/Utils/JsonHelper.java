7c816123d5311fa00f24f604656adcb02de3169a
martinsiitam <martin.siitam@hotmail.com>
2023-03-22 12:24:50 +0100

    gson support start - Martin

--- src/Main/java/Utils/JsonHelper.java
+++ src/Main/java/Utils/JsonHelper.java
@@ -1,4 +1,19 @@
 package Utils;
 
+import com.google.gson.Gson;
+
 public class JsonHelper {
+    public static Gson gson = new Gson();
+
+    public static String serializeObjectToJson(Object toSerialize){
+        return gson.toJson(toSerialize);
+    }
+
+    public static <T> T getObjectFromJson(String jsonOfObject, Class<T> classToReturn){
+        return gson.fromJson(jsonOfObject, classToReturn);
+    }
+
+    public static String readStringFromFile(String filepath){
+        return "";
+    }
 }
